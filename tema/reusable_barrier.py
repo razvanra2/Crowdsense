836bde47b9df4f7d1cac99fc03e29a52f56ac4b8
razvan <razvanradoi97@gmail.com>
2019-03-24 10:51:50 +0200

    add reusable barrier

--- tema/reusable_barrier.py
+++ tema/reusable_barrier.py
@@ -1,18 +1,24 @@
-from threading import Condition
-
+from threading import *
+ 
 class ReusableBarrier():
     def __init__(self, num_threads):
         self.num_threads = num_threads
-        self.count_threads = self.num_threads    # contorizeaza numarul de thread-uri ramase
-        self.cond = Condition()                  # blocheaza/deblocheaza thread-urile
-                                                 # protejeaza modificarea contorului
+        self.count_threads1 = [self.num_threads]
+        self.count_threads2 = [self.num_threads]
+        self.count_lock = Lock()                 # protejam accesarea/modificarea contoarelor
+        self.threads_sem1 = Semaphore(0)         # blocam thread-urile in prima etapa
+        self.threads_sem2 = Semaphore(0)         # blocam thread-urile in a doua etapa
  
     def wait(self):
-        self.cond.acquire()                      # intra in regiunea critica
-        self.count_threads -= 1;
-        if self.count_threads == 0:
-            self.cond.notify_all()               # deblocheaza toate thread-urile
-            self.count_threads = self.num_threads    # reseteaza contorul
-        else:
-            self.cond.wait();                    # blocheaza thread-ul eliberand in acelasi timp lock-ul
-        self.cond.release();                     # iese din regiunea critica
+        self.phase(self.count_threads1, self.threads_sem1)
+        self.phase(self.count_threads2, self.threads_sem2)
+ 
+    def phase(self, count_threads, threads_sem):
+        with self.count_lock:
+            count_threads[0] -= 1
+            if count_threads[0] == 0:            # a ajuns la bariera si ultimul thread
+                for i in range(self.num_threads):
+                    threads_sem.release()        # incrementarea semaforului va debloca num_threads thread-uri
+                count_threads[0] = self.num_threads  # reseteaza contorul
+        threads_sem.acquire()                    # num_threads-1 threaduri se blocheaza aici
+                                                 # contorul semaforului se decrementeaza de num_threads ori
